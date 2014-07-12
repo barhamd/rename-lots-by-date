c521bfa4be828877b3a5bf7612aa75395359e45d
Devin <devinbarham@gmail.com>
2014-07-12 15:34:54 -0600

    Some stuff

--- rename.rb
+++ rename.rb
@@ -7,7 +7,11 @@ directories = Dir.glob('**/*').select{ |fn| File.directory?(fn) }
 file_names = []
 directories.each do |directory|
   # If the entry is a file, add it to the file_names array
-  file_names << Dir.entries(directory).select { |f| File.file?(f) }
+  file_names << File.expand_path(Dir.entries(directory).select { |f| File.file?(f) })
+  file_names.flatten!
 end
 
+file_names.sort_by!{ |file_name| File.mtime(file_name) } # Sort array by creation time
+
+# Should probably name files by their unix date.
 puts file_names
