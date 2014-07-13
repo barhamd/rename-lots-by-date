b068f032c7b49393e777828244b629969a9426b6
Devin Barham <barhamd@gmail.com>
2014-07-13 16:46:49 -0600

    Just glob the files. Duh.

--- rename.rb
+++ rename.rb
@@ -1,17 +1,9 @@
 # Each image in each directory needs to be sorted.
 
-# Grab each directory
-directories = Dir.glob('**/*').select{ |fn| File.directory?(fn) }
-
-# Grab each file and put in an array
-file_names = []
-directories.each do |directory|
-  # If the entry is a file, add it to the file_names array
-  file_names << File.expand_path(Dir.entries(directory).select { |f| File.file?(f) })
-  file_names.flatten!
-end
+file_names = Dir.glob('**/*').select{ |fn| File.file?(fn) }
 
 file_names.sort_by!{ |file_name| File.mtime(file_name) } # Sort array by creation time
 
 # Should probably name files by their unix date.
 puts file_names
+puts "\n#{file_names.count} files found"
